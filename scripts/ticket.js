7af1eb342f6cde8439658d96769c5ee4fce4736e
Aminulislamovi <md.aminul4480@gmail.com>
2024-04-16 18:57:44 +0600

    added seat count and click ph paribahan

--- scripts/ticket.js
+++ scripts/ticket.js
@@ -1,8 +1,37 @@
-// .banner-image {
 
-//     background-image: url('../images/banner.png');
 
-//     background-repeat: no-repeat;
-//     height: 600px;
-//     margin: 10px;
-// }
+const addBtn =document.getElementsByClassName('add');
+let count =0;
+
+for (const btn  of addBtn){
+     btn.addEventListener('click', function(e){
+      count =count +1;
+    
+      const seatName =e.target.parentNode.childNodes[1].innerText;
+
+      const selectedContainer = document.getElementById('select-container');
+       const li =document.createElement('li')
+       const p=document.createElement('p')
+       p.innerText =seatName
+       
+       li.appendChild(p)
+       selectedContainer.appendChild(li);
+
+     
+
+      setInnerText('seat-quantity',count)
+      setInnerText('ticket-price',seatName)
+     })
+}
+
+
+function setInnerText (id , value){
+   document.getElementById(id).innerText=value;
+ 
+}
+
+function  btnClick (){
+   
+   const phSection2 =document.getElementById('ph-poribahan');
+   phSection2.classList.remove('hidden');
+}
